487f5d5a6ef544fdb2bd7952c57c8c9ee674ebbb
Kauã Segabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2025-11-16 07:54:10 -0300

    commitando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    // { path: 'status', component: StatusComponent, canActivate: [AuthGuard] },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'chat', component: ChatComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+];
